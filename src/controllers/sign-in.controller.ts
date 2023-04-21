be758de2e50ba9ca9b865a904f5236a317801c7a
yihaowang <yihao.wang@goanimate.com>
2023-04-21 16:38:04 +0800

    Add demain

--- src/controllers/sign-in.controller.ts
+++ src/controllers/sign-in.controller.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from 'express';
+import config from 'config';
+
+const DOMAIN = config.get('app.domain');
+const GOOGLE_CLIENT_ID = config.get('auth.google.clientId');
+const GOOGLE_AUTH_CALLBACK_POSTFIX = config.get('auth.google.callbackUrl');
+const GOOGLE_AUTH_CALLBACK = `${DOMAIN}${GOOGLE_AUTH_CALLBACK_POSTFIX}`;
 
 class SignInController {
   renderSignInPage(req: Request, res: Response) {
-    res.render('signin', { title: 'sing-dash' });
+    res.render('signin', {
+      title: 'sing-dash',
+      googleAuthCallback: GOOGLE_AUTH_CALLBACK,
+      googleClientId: GOOGLE_CLIENT_ID,
+    });
   }
 }
 
