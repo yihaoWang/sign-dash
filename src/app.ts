537a8c6e73c938aba174d67433927f591686991c
yihaowang <yihao.wang@goanimate.com>
2023-04-24 22:39:27 +0800

    Use Bootstrap

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,6 @@ app.use(session({
 }));
 
 app.set('views', path.join(__dirname, 'views'));
-app.use('/static', express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'ejs');
 
 router.forEach((route) => {
